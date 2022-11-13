6b5010c8217cb427e855f307e0f267c0fa005489
Roman <madara-09@bk.ru>
2022-11-13 13:23:49 +0300

    feat: реализована логика работы с dto

--- src/main/java/ru/yandex/practicum/filmorate/mapper/UserMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/UserMapper.java
@@ -1,13 +1,30 @@
 package ru.yandex.practicum.filmorate.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 import ru.yandex.practicum.filmorate.dto.UserDto;
 import ru.yandex.practicum.filmorate.model.User;
 
-@Mapper
-public interface UserMapper {
-    UserMapper mapper = Mappers.getMapper(UserMapper.class);
-    User toUser(UserDto userDto);
-    UserDto toUserDto(User user);
+public class UserMapper {
+    public static UserDto mapToUserDto(User user) {
+        UserDto userDto = UserDto.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .login(user.getLogin())
+                .birthday(user.getBirthday())
+                .build();
+        userDto.getFriends().addAll(user.getFriends());
+        return userDto;
+    }
+
+    public static User mapToUser(UserDto userDto) {
+        User user = User.builder()
+                .id(userDto.getId())
+                .name(userDto.getName())
+                .email(userDto.getEmail())
+                .login(userDto.getLogin())
+                .birthday(userDto.getBirthday())
+                .build();
+        user.getFriends().addAll(userDto.getFriends());
+        return user;
+    }
 }
